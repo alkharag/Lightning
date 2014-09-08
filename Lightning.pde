30881b62d798aa2f31d5e302aa3fb72bda102ebb
alkharag <kharag.alex_sa@yahoo.com>
2014-09-08 11:07:00 -0700

    New program

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,51 @@
+int startX = 250;
+int startY = 0;
+int endX = 0;
+int endY = 0;
+int i = 0;
+
 void setup()
 {
-  size(300,300);
+	background(0,0,0);
+	stroke(0, 150 +i*5, 240 -i*2);	
+	strokeWeight(1);
+	size(500,500);
+	frameRate(250);
 }
 void draw()
 {
+	strokeWeight(3);
+	while (endY < 500) {
+		endX = startX + (int)(Math.random()*18-9);
+		endY = startY + (int)((Math.random()*10));
+
+
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
+	}
+	if (endY > 499) {
+		startX = 250; //(int)(Math.random()*300);
+		startY = 0;
+		endX = 0;
+		endY = 0;
+		i++;
+	}
+	if(i > 3){
+		background(0,0,0);
+		i = 0;
+	}
+	
 
 }
-void mousePressed()
+/*void mousePressed()
 {
-
-}
+	
+	startX = 0;
+	startY = 0;
+	endX = 0;
+	endY = 0;
+	
+	
+}*/
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,20 +14,56 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 250;
+int startY = 0;
+int endX = 0;
+int endY = 0;
+int i = 0;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	background(0,0,0);
+	stroke(0, 150 +i*5, 240 -i*2);	
+	strokeWeight(1);
+	size(500,500);
+	frameRate(250);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	strokeWeight(3);
+	while (endY < 500) {
+		endX = startX + (int)(Math.random()*18-9);
+		endY = startY + (int)((Math.random()*10));
+
+
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
+	}
+	if (endY > 499) {
+		startX = 250; //(int)(Math.random()*300);
+		startY = 0;
+		endX = 0;
+		endY = 0;
+		i++;
+	}
+	if(i > 3){
+		background(0,0,0);
+		i = 0;
+	}
+	
+
 }
-public void mousePressed()
+/*void mousePressed()
 {
-  redraw();
-}
+	
+	startX = 0;
+	startY = 0;
+	endX = 0;
+	endY = 0;
+	
+	
+}*/
 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
